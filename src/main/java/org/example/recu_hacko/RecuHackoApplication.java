b7b283f96add964a1f264e42d2384a7f1444ff48
BFishyUt3c <benjamin.suarez@utec.edu.pe>
2025-05-13 16:45:32 -0500

    Mi primer commit

---
+++ src/main/java/org/example/recu_hacko/RecuHackoApplication.java
@@ -0,0 +1,13 @@
+package org.example.recu_hacko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecuHackoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecuHackoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/recu_hacko/RecuHackoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.recu_hacko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecuHackoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
